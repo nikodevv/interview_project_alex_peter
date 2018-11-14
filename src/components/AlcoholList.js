5f9157fb50110b560e6afda4322a9b24a2591662
nikodevv <niko.oshinov@gmail.com>
2018-11-14 17:44:36 -0500

    Added LCBO API support and made placeholder component which will build list

--- src/components/AlcoholList.js
+++ src/components/AlcoholList.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Row, Col} from 'reactstrap';
 import PropTypes from 'prop-types';
-
+import $ from 'jquery';
+import {API_KEY} from '../misc/API_KEY';
 /**
  * A list of alcohol "Item"s.
  */
-const AlcoholList = (props) => {
-    return (<p>{props.searchStr}</p>
-    );
+class AlcoholList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {loadedItems: []}
+    };
+
+    /**
+     * Makes API call to LCBO for desired product.
+     * and calls abstract factory function with
+     * returned data to create appropriate list type.
+     * @param {string} str_ - Search string for query
+     */
+    getItems(str_){
+        $.ajax({
+            url: `//lcboapi.com/products?name=${str_}`,
+            type: 'get',
+            dataType: "json",
+            headers: {
+                'Authorization': `Token token="${API_KEY}"`
+            },
+            async: true,
+            success: (data) => {
+                console.log(data)
+                data.results.forEach(this.createAlcoholItem);
+            }
+        });
+    };
+    componentDidMount(){
+        this.getItems()
+    }
+    createAlcoholItem(){
+        let newLoadedItems = this.state.loadedItems.slice();
+        this.setState(Object.assign({}, {loadedItems: newLoadedItems}));
+    }
+    render () {
+        return (
+            <div>{this.state.loadedItems}</div>
+        );
+    }
+};
+
+const AlcoholItem = (props) =>{
+    return(
+        <Row>
+            <Col>props.name</Col>
+
+        </Row>
+    )
 };
 
 AlcoholList.propTypes = {
@@ -14,7 +61,7 @@ AlcoholList.propTypes = {
      * String used to generate API query string.
      * Initial prop is expected to be null.
      */
-    searchStr: PropTypes.oneOfType([PropTypes.string, null]).isRequired
+    searchStr: PropTypes.string
 }
 
 export {AlcoholList};
---
+++ src/misc/API_KEY.js
@@ -0,0 +1,3 @@
+// On a real project I would never commit this file, just an example of it.
+const API_KEY = "MDo1YjViYThlYS1lN2NhLTExZTgtOTE4NS02ZmI2ZDgxNWE1Mzg6WE1WWmwzUHBVZ08zZTBYa3BmRFhmYVBCSE5MOVNtRzZ0N29x";
+export {API_KEY}
