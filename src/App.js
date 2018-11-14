ae7b5b876060bdf49d87bd60823a69dad6dd1a56
nikodevv <niko.oshinov@gmail.com>
2018-11-13 19:26:38 -0500

    Added file for searchbar component @ top for page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import {Container, Row} from 'reactstrap';
+import {SearchBar} from './components/SearchBar'
 
 class App extends Component {
     render(){
         return (
             <Container>
-                <Row className="SearchBarContainer sticky-top">
-                    .
-                </Row>
+                <SearchBar></SearchBar>
                 <div className="test-sizing"></div>
             </Container>
         );
---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Row} from 'reactstrap';
+
+class SearchBar extends Component {
+    render(){
+        return (
+                <Row className="SearchBarContainer sticky-top">
+                    .
+                </Row>
+        );
+    }
+};
+
+export {SearchBar};
