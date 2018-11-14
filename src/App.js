1a7f35ded97843cd738e2bddedb9956007c42eab
nikodevv <niko.oshinov@gmail.com>
2018-11-13 21:38:39 -0500

    Made searchbar take callback from parent that is called on keypress. App gives callback to searchbar that is used to store input value in state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import {Container, Row} from 'reactstrap';
 import {SearchBarRow} from './components/SearchBar'
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {searchStr: null};
+        this.getSearchOnEnter = this.getSearchOnEnter.bind(this);
+    };
+
+    /**
+     * Stores input string to state.
+     * @param {KeyboardEvent} e
+     */
+    getSearchOnEnter(e){
+        this.setState({searchStr: e.target.value})
+    };
+
     render(){
         return (
             <Container>
-                <SearchBarRow/>
-                <div className="test-sizing"></div>
+                <SearchBarRow eventHandler={this.getSearchOnEnter}/>
+                <Row>{this.state.searchStr}</Row>
             </Container>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,56 @@
-import React, {Component} from 'react';
-import {Row, Col, Form, Input, Label} from 'reactstrap';
+import React from 'react';
+import {Row, Col, Input} from 'reactstrap';
+import PropTypes from 'prop-types';
 
-class SearchBarRow extends Component {
-    render(){
-        return (
-                <Row className="SearchBarContainer sticky-top">
-                    <Col lg="6"className="children-along-bot">
-                        <h1 className="title-header">
-                            The Liquor Journal
-                        </h1>
-                    </Col>
-                    <Col lg="1"/>
-                    <Col lg="5" className="children-along-bot">
-                        <h1><SearchBar></SearchBar></h1>
-                    </Col>
-                </Row>
-        );
-    }
-};
-const SearchBar = () => {
+/**
+* The navbar-like component @ top of page which contains
+* the searchbar itself.
+*/
+const SearchBarRow = (props) => {
     return (
-        <Form>
-            <Input placeholder="Search for Alcohol"/>
-        </Form>
-    )
-}
+            <Row className="SearchBarContainer sticky-top">
+                <Col lg="6"className="children-along-bot">
+                    <h1 className="title-header">
+                        The Liquor Journal
+                    </h1>
+                </Col>
+                <Col lg="1"/>
+                <Col lg="5" className="children-along-bot">
+                    <h1><SearchBar eventHandler={props.eventHandler}></SearchBar></h1>
+                </Col>
+            </Row>
+    );
+};
+
+const SearchBar = (props) => {
+    
+    /**
+     * A wrapper that calls callback only on
+     * certain keypresses.
+     * @param {KeyboardEvent} event 
+     */
+    const callEventHandler = (event) => {
+        if (event.key =="Enter"){
+            props.eventHandler(event);
+        };
+    };
+
+    return (
+        <Input placeholder="Search for Alcohol" onKeyPress={callEventHandler}/>
+    );
+};
+
+SearchBarRow.propTypes = {
+    /** callback function passed to SearchBar */
+    eventHandler: PropTypes.func.isRequired
+};
+
+SearchBar.propTypes = {
+    /**
+     * Callback function that is used to read
+     * input box value by parent components.
+     */
+    eventHandler: PropTypes.func.isRequired
+};
+
 export {SearchBarRow};
